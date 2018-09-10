ca2706ede9987d3c6117b6de20af7bfa6af852f1
Lucas DiCioccio <lucas@dicioccio.fr>
2018-09-10 22:04:04 +0200

    Add bidi and general handlers.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
 module Lib where
@@ -5,18 +6,22 @@ module Lib where
 import Network.GRPC.Server
 
 import Control.Concurrent (threadDelay)
+import Control.Monad (void)
 import Data.ProtoLens.Message (def)
 import Network.Wai.Handler.WarpTLS (defaultTlsSettings)
 import Network.Wai.Handler.Warp (defaultSettings)
 import Network.GRPC.HTTP2.Types (RPC(..))
 import Network.GRPC.HTTP2.Encoding (gzip)
 import Proto.Protos.Grpcbin (GRPCBin, EmptyMessage(..), IndexReply(..), IndexReply'Endpoint(..))
+import System.Environment (getArgs)
 
 someFunc :: IO ()
-someFunc = runGrpc defaultTlsSettings defaultSettings handlers [gzip]
+someFunc = do
+    args <- getArgs
+    runGrpc defaultTlsSettings defaultSettings (handlers args) [gzip]
 
-handlers :: [ServiceHandler]
-handlers =
+handlers :: [String] -> [ServiceHandler]
+handlers args =
   [ unary (RPC :: RPC GRPCBin "empty") handleEmpty
   , unary (RPC :: RPC GRPCBin "index") handleIndex
   , unary (RPC :: RPC GRPCBin "specificError") handleSpecificError
@@ -24,6 +29,9 @@ handlers =
   , unary (RPC :: RPC GRPCBin "dummyUnary") handleDummyUnary
   , serverStream (RPC :: RPC GRPCBin "dummyServerStream") handleDummyServerStream
   , clientStream (RPC :: RPC GRPCBin "dummyClientStream") handleDummyClientStream
+  , if "--bidistep" `elem` args
+      then bidiStream (RPC :: RPC GRPCBin "dummyBidirectionalStreamStream") handleDummyBiDiStreamStep
+      else generalStream (RPC :: RPC GRPCBin "dummyBidirectionalStreamStream") handleDummyBiDiGeneralStream
   ]
 
 handleIndex :: UnaryHandler GRPCBin "index"
@@ -67,3 +75,28 @@ handleDummyClientStream _ = do
     return $ (0, ClientStream
                      (\n input -> print ("cstream-msg"::[Char], n, input) >> return (n+1))
                      (\n -> print ("cstream-end"::[Char], n) >> return def))
+
+handleDummyBiDiStreamStep :: BiDiStreamHandler GRPCBin "dummyBidirectionalStreamStream" Int
+handleDummyBiDiStreamStep _ = do
+    print ("bidistream-start"::[Char])
+    return $ (10, BiDiStream go)
+  where
+    go 0                  = putStrLn "bidistream-abort" >> pure Abort
+    go n | n `mod` 2 == 0 = pure $ WaitInput
+                (\m msg -> print ("bidistream-msg" :: [Char], m, msg) >> pure (m - 1))
+                (\m -> print ("bidistream-closed" :: [Char], m) >> pure 0)
+         | otherwise      = do
+                print ("bidistream-out"::[Char], n)
+                pure $ WriteOutput (n -1) def
+
+handleDummyBiDiGeneralStream :: GeneralStreamHandler GRPCBin "dummyBidirectionalStreamStream" Int Int
+handleDummyBiDiGeneralStream _ = do
+    print ("general-bidistream-start"::[Char])
+    return (0, incoming, 10, outgoing)
+  where
+    incoming = IncomingStream handleMsg handleEof
+    handleMsg n msg = print ("general-msg" :: [Char], n, msg) >> pure (n+1)
+    handleEof n     = print ("general-eof" :: [Char], n)
+    outgoing = OutgoingStream (\n -> print ("general-out"::[Char], n) >> getNext n)
+    getNext 0 = pure Nothing
+    getNext n = pure (Just (n-1, def))
